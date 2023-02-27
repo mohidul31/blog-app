e15e92e90b084934f5bf14eb2ee45177abbdd8c4
shovon487 <mislam6789@gmail.com>
2023-02-27 13:14:07 +0600

    delete problem updated

--- routes/api.php
+++ routes/api.php
@@ -29,5 +29,5 @@ Route::middleware(['auth:api'])->group(function () {
     Route::get('/articles', [ArticleController::class, 'index']);    
     Route::post('/articles', [ArticleController::class, 'store']);
     Route::put('/articles/{id}', [ArticleController::class, 'update']);
-    Route::delete('/articles', [ArticleController::class, 'destroy']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
 });
